fb296ca92d12141ec561ebb6e3893fa0aac53e26
jwritchie <JRitchie.NC@gmail.com>
2017-10-24 14:53:49 -0400

    Initial Commit

---
+++ jritchieFinancialPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jritchieFinancialPortal.Startup))]
+namespace jritchieFinancialPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
