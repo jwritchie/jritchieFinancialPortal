329ec6e87f3adaff39ac56a18c67401e328f286e
jwritchie <JRitchie.NC@gmail.com>
2017-11-14 12:15:35 -0500

    Dashboard

--- jritchieFinancialPortal/Controllers/HomeController.cs
+++ jritchieFinancialPortal/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using jritchieFinancialPortal.Models.Helpers;
+﻿using jritchieFinancialPortal.Models;
+using jritchieFinancialPortal.Models.CodeFirst;
+using jritchieFinancialPortal.Models.Helpers;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -20,11 +22,16 @@ namespace jritchieFinancialPortal.Controllers
         }
 
 
-        [AuthorizeHouseholdRequired]
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //[AuthorizeHouseholdRequired]
+        //public ActionResult Index()
+        //{
+        //    var currentUserHousehold = User.Identity.GetHouseholdId();
+        //    List<Transaction> currentUserTransactions = new List<Transaction>();
+        //    currentUserTransactions = db.Transactions.Where(t => t.Account.HouseholdId == currentUserHousehold).ToList();
+
+        //    return View(currentUserTransactions);
+        //}
+
 
         [AuthorizeHouseholdRequired]
         public ActionResult About()
@@ -42,5 +49,45 @@ namespace jritchieFinancialPortal.Controllers
 
             return PartialView();
         }
+
+
+        // GET: DashBoard/Index
+        [AuthorizeHouseholdRequired]
+        public ActionResult Index()
+        {
+            // Build & populate DashTransactionsViewModel
+            List<DashTransactionsViewModel> dashViewModel = new List<DashTransactionsViewModel>();
+
+            int currentUserHouseholdId = User.Identity.GetHouseholdId().Value;
+            int expenseTypeId = db.TransactionTypes.First(t => t.Name == "Expense").Id;
+            int incomeTypeId = db.TransactionTypes.First(t => t.Name == "Income").Id;
+
+            List<BankAccount> currentUserBankAccounts = db.BankAccounts.Where(b => b.HouseholdId == currentUserHouseholdId).ToList();
+
+            foreach(var account in currentUserBankAccounts)
+            {
+                DashTransactionsViewModel currentAccountVM = new DashTransactionsViewModel();
+
+                decimal currentExpenseTotal = account.Transactions.Where(t => t.Category.TransactionTypeId == expenseTypeId).Sum(t => (decimal?)t.Amount) ?? 0;
+                decimal currentIncomeTotal = account.Transactions.Where(t => t.Category.TransactionTypeId == incomeTypeId).Sum(t => (decimal?)t.Amount) ?? 0;
+
+                currentAccountVM.AccountName = account.Name;
+                currentAccountVM.TotalExpenseTransactions = currentExpenseTotal * -1;
+                currentAccountVM.TotalIncomeTransactions = currentIncomeTotal;
+
+                dashViewModel.Add(currentAccountVM);
+            }
+
+            return View(dashViewModel);
+
+
+            //decimal currentExpenseTransactionsTotal = db.Transactions.Where(t => t.Account.HouseholdId == currentUserHouseholdId).Where(t => t.Category.TransactionTypeId == 1).Sum(t => (decimal?)t.Amount) ?? 0;
+            //decimal currentIncomeTransactionsTotal = db.Transactions.Where(t => t.Account.HouseholdId == currentUserHouseholdId).Where(t => t.Category.TransactionTypeId == 2).Sum(t => (decimal?)t.Amount) ?? 0;
+
+            //dashViewModel.TotalExpenseTransactions = currentIncomeTransactionsTotal * -1;
+            //dashViewModel.TotalIncomeTransactions = currentIncomeTransactionsTotal;
+            //return View(dashViewModel);
+        }
+
     }
 }
---
+++ jritchieFinancialPortal/Models/DashTransactionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using jritchieFinancialPortal.Models.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jritchieFinancialPortal.Models
+{
+    public class DashTransactionsViewModel
+    {
+        //public List<Transaction> IncomeTransactions { get; set; }
+        //public List<Transaction> ExpenseTransactions { get; set; }
+        public string AccountName { get; set; }
+        public decimal TotalIncomeTransactions { get; set; }
+        public decimal TotalExpenseTransactions { get; set; }
+    }
+}
